492797c1b645a006af2222e3a00e89f82a314951
nagibator228777 <dobryash328@gmail.com>
2025-04-24 10:18:35 +0300

    add tests

---
+++ tests/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+
+@pytest.fixture
+def transaction_data() -> list[dict]:  # Аннотация типа для возвращаемого значения
+    """Фикстура с тестовыми данными."""
+    return [
+        {"date": "2023-10-27T10:00:00.000Z", "state": "EXECUTED"},
+        {"date": "2023-10-26T10:00:00.000Z", "state": "EXECUTED"},
+        {"date": "2023-10-28T10:00:00.000Z", "state": "CANCELED"},
+        {"date": "2023-10-27T12:00:00.000Z", "state": "EXECUTED"},  # Другое время
+        {"date": "2023-10-27T10:00:00.000Z", "state": "PENDING"},
+    ]
+
